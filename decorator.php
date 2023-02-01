87d51a41a1316a6e9dbab63783a60ab9928c8cf0
student43120 <101247032+student43120@users.noreply.github.com>
2023-02-01 22:11:43 +0100

    Upload decorator
    upload designed pattern -> decorator

---
+++ decorator.php
@@ -0,0 +1,66 @@
+<?php
+
+interface laptopService
+{
+    public function getCost();
+
+    public function getListtoDo();
+}
+
+class Overview implements laptopService
+{
+    public function getCost()
+    {
+        return 20;
+    }
+
+    public function getListtoDo()
+    {
+        return 'Laptop health check';
+    }
+}
+
+class batteryChange implements laptopService
+{
+    protected $laptopService;
+
+    public function __construct(laptopService $laptopService)
+    {
+        $this->laptopService = $laptopService;
+    }
+
+
+    public function getCost()
+    {
+        return 5 + $this->laptopService->getCost();
+    }
+
+    public function getListtoDo()
+    {
+        return $this->laptopService->getListtoDo() . ', and battery change ';
+    }
+}
+
+class keyboardChange implements laptopService
+{
+    protected $laptopService;
+
+    public function __construct(laptopService $laptopService)
+    {
+        $this->laptopService = $laptopService;
+    }
+
+
+    public function getCost()
+    {
+        return 14 + $this->laptopService->getCost();
+    }
+
+    public function getListtoDo()
+    {
+        return $this->laptopService->getListtoDo() . ', and keyboard change';
+    }
+}
+
+$service = new batteryChange(new keyboardChange(new Overview()));
+echo $service->getListtoDo();
